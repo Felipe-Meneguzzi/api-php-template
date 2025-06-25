c1bd46d2fd1785119a10f4e6124fef5af0c2493f
Felipe Meneguzzi <felipe.meneguzzi@universo.univates.br>
2025-06-25 09:00:44 -0300

    feature: inicio de implementação de observabilidade com OpenTelemetry

--- container-conf/entrypoint.sh
+++ container-conf/entrypoint.sh
@@ -6,8 +6,12 @@ if [ ! -d "/var/www/html/vendor" ]; then
   echo "Pasta vendor não encontrada. Executando composer install..."
   composer install --no-dev --optimize-autoloader
   composer dump-autoload
-  ./vendor/bin/openapi src -o public/openapi.json
 fi
+
+# if [ ! -d "/var/www/html/vendor/bin/openapi" ]; then
+#   echo "Openapi encontrado. Gerando documentação automatizada..."
+#   /var/www/html/vendor/bin/openapi src -o public/openapi.json
+# fi
 
 # Executa o comando padrão (como iniciar o Apache)
 exec "$@"
